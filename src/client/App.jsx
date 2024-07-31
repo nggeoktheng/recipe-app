3d5f891d79ac7434ccdd94ad0698188ef6533185
Ng Geok Theng <gthengng@gmail.com>
2024-08-01 11:36:52 +1200

    Add basic login and signup page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -5,11 +5,21 @@ import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/Signup",
+    element: <Signup />
   }
 ])
 
