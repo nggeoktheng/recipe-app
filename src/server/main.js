b38f07036bd83961019348f91efc9275e813e1ca
Ng Geok Theng <gthengng@gmail.com>
2024-08-01 13:03:14 +1200

    Add dotenv to load environment file and add .env.sample file

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,20 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
+ViteExpress.config({
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+});
+
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log(`Server is listening on port ${port}`),
 );
